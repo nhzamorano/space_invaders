f73711106a2dbcc3d14d04648d45cc2c292d09ad
Natanael Herrera <nhzamorano@hotmail.com>
2024-05-29 21:13:16 -0500

    Funciones KeyDown y KeyUp

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """
+    A class to store all setting for Alien Invasion
+    """
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 600 
+        self.bg_color = (250,240,210)
